2c83af8c82749f5948cc81f5bba57720b35f5f1a
ChhunlinOn <chhunlin.on@institute.pse.ngo>
2025-04-23 16:50:41 +0700

    branch checkin

---
+++ src/controllers/checkinController.ts
@@ -0,0 +1,59 @@
+import e, { NextFunction, Request, Response } from "express";
+import { Checkin, CheckinService } from "../interfaces/checkinInterface";
+import { Invite, InviteService } from "../interfaces/Inviteinterface";
+
+export class CheckinController {
+  private checkinService: CheckinService;
+  private inviteService: InviteService;
+
+  constructor(checkinService: CheckinService, inviteService: InviteService) {
+    this.checkinService = checkinService;
+    this.inviteService = inviteService;
+  }
+
+  async getAllCheckinsByEventID(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { event_id } = req.params; // Extract event_id from route parameters
+      console.log("event_id:", event_id);
+      const result = await this.checkinService.findAllCheckinsByEventID(event_id);
+      res.json({ message: "Get all checkins.", data: result });
+      return;
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async createCheckin(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ){
+    try {
+    //   const { event_id,in } = req.params;
+    //   console.log("event_id:", event_id);
+    //   console.log("req.userId", req.userId);
+
+      const { invite_id,event_id } = req.body;
+      if (!invite_id||!event_id) {
+        throw new Error("The 'invite_id' and 'event_id' fields are required.");
+      }
+
+      // Check if the invite exists
+    //   const invite: Invite | null = await this.inviteService.getInviteById(
+    //     invite_id
+    //   );
+    //   if (!invite) {
+    //     throw new Error("Invite not found.");
+    //   }
+
+      const result = await this.checkinService.createCheckin({
+        event_id,
+        invite_id,
+        // userId: req.userId,
+      });
+      res.json({ message: "Create checkin.", data: result });
+    } catch (error) {
+      next(error);
+    }
+  }
+}
---
+++ src/repositories/postgres/checkinRepository.ts
@@ -0,0 +1,38 @@
+import { Pool } from "pg";
+import { Checkin, CheckinRepository } from "../../interfaces/checkinInterface";
+import { queryWithLogging } from "./utils";
+
+export class PostgresCheckinRepository implements CheckinRepository {
+  private pool: Pool;
+
+  constructor(pool: Pool) {
+    this.pool = pool;
+  }
+    async create(checkin: Omit<Checkin, "id">): Promise<Checkin> {
+        return queryWithLogging(
+            this.pool,
+            `INSERT INTO public.checkin (event_id, invite_id) VALUES ($1, $2) RETURNING id, event_id, invite_id`,
+            [checkin.event_id, checkin.invite_id]
+        ).then((result) => {
+            if (result.rows.length === 0) {
+                throw new Error("Checkin not created");
+            }
+            return result.rows[0];
+        }
+        );
+    }
+    findCheckinByInviteID(inviteID: string): Promise<Checkin | null> {
+        throw new Error("Method not implemented.");
+    }
+    getAllCheckins(): Promise<Checkin[] | null> {
+        throw new Error("Method not implemented.");
+    }
+
+  async findAllCheckinsByEventID(id: string): Promise<Checkin[] | null> {
+    return queryWithLogging(
+      this.pool,
+      "SELECT id, event_id, invite_id FROM public.checkin WHERE event_id = $1",
+      [id]
+    ).then((result) => result.rows || null);
+  }
+}
